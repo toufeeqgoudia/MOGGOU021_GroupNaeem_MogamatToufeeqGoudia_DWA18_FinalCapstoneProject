d70c443f4e5990dff6728c96dc03d88af70e3a5f
toufeeqgoudia <toufeeqgoudia@gmail.com>
2023-07-20 22:30:44 +0200

    added back and home button functionality

--- src/Components/DiscoverList/DiscoverList.jsx
+++ src/Components/DiscoverList/DiscoverList.jsx
@@ -1,27 +1,11 @@
-import { useState, useEffect } from 'react'
-import Slider from 'react-slick';
+// import { useState, useEffect } from 'react'
+// import Slider from 'react-slick';
 import PropTypes from 'prop-types'
 
 export default function DiscoverList({ shows }) {
-    const [randomPodcasts, setRandomPodcasts] = useState([])
-    
-    useEffect(() => {
-        
-    }, [])
 
     return (
-        <div>
-        <h2>Random Podcasts</h2>
-        <Slider>
-          {randomPodcasts.map((podcast) => (
-            <div key={podcast.id}>
-              <h3>{podcast.title}</h3>
-              <p>{podcast.description}</p>
-              <img src={podcast.image} alt={podcast.title} />
-            </div>
-          ))}
-        </Slider>
-      </div>
+        <div>Hello World!</div>
     )
 }
 
--- src/Components/Nav/BottomNav.jsx
+++ src/Components/Nav/BottomNav.jsx
@@ -1,15 +1,18 @@
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import HomeIcon from '@mui/icons-material/Home';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 export default function BottomNav() {
+  const navigate = useNavigate()
+
   return (
     <nav className="fixed -bottom-0 -left-0 -right-0 h-10 flex justify-around bg-white z-10">
-        <Button>
+        <Button onClick={() => navigate(-1)}>
           <ArrowBackIcon className="text-xl" />
         </Button>
-        <Button>
+        <Button onClick={() => navigate('/')}>
           <HomeIcon className="text-xl" />
         </Button>
         <Button>
