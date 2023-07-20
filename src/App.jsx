fdc5bd99e8db2710deadd8b739baf8daa6c3c6c6
toufeeqgoudia <toufeeqgoudia@gmail.com>
2023-07-21 00:23:03 +0200

    working on home page carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
-import TopNav from './Components/Nav/TopNav';
-import BottomNav from './Components/Nav/BottomNav';
+// import TopNav from './Components/Nav/TopNav';
+// import BottomNav from './Components/Nav/BottomNav';
 import ShowList from './Components/ShowList/ShowList';
-import EpisodeList from './Components/EpisodeList/EpisodeList';
+// import EpisodeList from './Components/EpisodeList/EpisodeList';
 // import Home from './Pages/Home/Home';
 // import Login from './Pages/Login/Login'
 
 const App = () => {
   return (
     <>
-      <TopNav />
+      <ShowList />
+      {/* <TopNav />
       <Routes>
         <Route path="/" element={<ShowList />} />
         <Route path="/:showId/:season" element={<EpisodeList />} />
       </Routes>
-      <BottomNav />
+      <BottomNav /> */}
     </>
   );
 }
--- src/Components/DiscoverList/DiscoverList.jsx
+++ src/Components/DiscoverList/DiscoverList.jsx
@@ -1,16 +1,71 @@
-// import { useState, useEffect } from 'react'
-// import Slider from 'react-slick';
-import PropTypes from 'prop-types'
+import { useEffect, useState } from "react";
+import { genreMapping } from "../../Utils/genreMapping";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import PropTypes from "prop-types";
 
 const DiscoverList = ({ shows }) => {
+  const [randomShows, setRandomShows] = useState([]);
 
-    return (
-        <div>Hello World!</div>
-    )
-}
+  useEffect(() => {
+    const getRandomPodcasts = (count) => {
+      const shuffledPodcasts = shows.sort(() => Math.random() - 0.5);
+      const randomPodcasts = shuffledPodcasts.slice(0, count);
+      return randomPodcasts;
+    };
+
+    const numberOfPodcasts = 10;
+    const randomPodcasts = getRandomPodcasts(numberOfPodcasts);
+    setRandomShows(randomPodcasts);
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="mt-20">
+      <Slider {...settings}>
+        {randomShows.map((show) => (
+          <div
+            key={show.id}
+            className="w-44 h-80 m-20px mx-auto mb-2 bg-gray-300 rounded-lg text-left cursor-pointer object-fill border-none"
+            onClick={() => handleShow(show.id)}
+          >
+            <img
+              src={show.image}
+              alt={show.title}
+              className="w-full h-42 top-0 rounded-lg"
+            />
+            <h3 className="text-sm px-1.5 py-1 font-bold">{show.title}</h3>
+            <p className="text-xs px-1.5 pb-1">
+              {show.seasons} Season{show.seasons <= 1 ? "" : "s"}
+            </p>
+            <p className="text-xs px-1.5 pb-1">
+              {show.genres.map((genreId) => genreMapping[genreId]).join(", ")}
+            </p>
+            <p className="text-xs px-1.5 pb-1">
+              Last Updated:{" "}
+              {new Date(show.updated).toLocaleDateString("en-GB", {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              })}
+            </p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
 DiscoverList.propTypes = {
-    shows: PropTypes.array
-}
+  shows: PropTypes.array,
+};
 
-export default DiscoverList
+export default DiscoverList;
