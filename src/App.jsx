476172b272c4ef31a8238b54dbfe9658e25e9cc3
toufeeqgoudia <toufeeqgoudia@gmail.com>
2023-07-29 21:44:19 +0200

    changed redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import ProtectedRoute from "./Components/ProtectedRoute";
-import { useEffect } from "react";
 
 const App = () => {
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    navigate('/login')
-  }, [navigate])
-
   return (
       <Routes>
         <Route path="/login" element={<Login />} />
